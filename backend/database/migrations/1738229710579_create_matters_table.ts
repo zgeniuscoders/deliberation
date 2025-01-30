c7fb967ad5e7cebf3a93093ca0dbec2fb8a3cca3
zgeniuscoders <zgeniuscoders.com>
2025-01-30 11:16:33 +0100

    creation de migration

---
+++ backend/database/migrations/1738229710579_create_matters_table.ts
@@ -0,0 +1,34 @@
+import {BaseSchema} from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'matters'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.string("name")
+      table.string("code")
+
+      table.integer("category_id")
+        .unsigned()
+        .references("categories.id")
+
+      table.integer("major_id")
+        .unsigned()
+        .references("majors.id")
+
+      table.integer("semester_id")
+        .unsigned()
+        .references("semesters.id")
+
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
